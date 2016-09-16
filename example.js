2b2c5aa5e788d6d9e3b608eb1eb96cd0e3366001
Bharat Patil <bharatpatil@MacBook-Pro.local>
2016-09-16 14:54:49 +0530

    added examples

--- example.js
+++ example.js
@@ -2,7 +2,7 @@ var ly = require('./lib/loop-yield');
 
 
 var itr = function(index) {
-    console.log(index);
+    console.log('a' + index);
 }
 
 var itrDone = function() {
@@ -11,11 +11,23 @@ var itrDone = function() {
 
 
 var a = new ly({
-    totalItems: 1000000,
+    totalItems: 127,
     iterator: itr,
     iterationDone: itrDone,
-    yieldTime: 50,
-    chunks: 100
+    yieldTime: 500,
+    chunkSize: 10
 });
 
 a.loadBalancer();
+
+var b = new ly().setOptions({
+    totalItems: 1000,
+    iterator: (index) => {
+        console.log('b ' + index);
+    },
+    iterationDone: itrDone,
+    yieldTime: 500,
+    chunkSize: 25
+});
+
+b.loadBalancer();
